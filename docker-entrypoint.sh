ec61e10d8ed591146fadcaa259da4a0b6ee490a1
3DA4C300 <3DA4C300@protonmail.com>
2020-04-15 19:58:49 +0200

    Fix trap for sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f $HOME/.safecoin/safecoin.conf ]; then
     echo "SafeNode hasn't been configured yet. Follow these steps: https://github.com/Fair-Exchange/safenode-docker/#Configure-the-container"
-    trap "exit 1" SIGTERM
+    trap "exit 1" TERM
     while :; do sleep 1; done
 fi
 
